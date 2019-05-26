8a0ea67966288127796e026f75f30f61f5d68ee4
LongVH <vuhailongkl97@gmail.com>
2019-05-26 22:51:14 +0700

    update spi driver

---
+++ spi/simple_spi/main.mod.c
@@ -0,0 +1,39 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x977e40b4, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xa60f790b, __VMLINUX_SYMBOL_STR(driver_unregister) },
+	{ 0x3eaabd36, __VMLINUX_SYMBOL_STR(__spi_register_driver) },
+	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xb1ad28e0, __VMLINUX_SYMBOL_STR(__gnu_mcount_nc) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+MODULE_ALIAS("of:N*T*Ctestlongvh12");
+MODULE_ALIAS("of:N*T*Ctestlongvh12C*");
