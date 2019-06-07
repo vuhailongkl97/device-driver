2ac8c413b8d7d9dd31a0bebb1892729df2650ac7
Demo User <debian@beaglebone.localdomain>
2019-06-07 15:13:46 +0000

    del trash

--- device_tree_overlay_example/build_dts_overlay.sh
+++ device_tree_overlay_example/build_dts_overlay.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "get device tree > dt"
-dtc -I fs /sys/firmware/devicetree/base > dt
+#dtc -I fs /sys/firmware/devicetree/base > dt
 echo "build .dtbo"
 dtc -O dtb -o BB-SPI0-01-00A0.dtbo -b 0 -@ BB-SPI0-01-00A0.dts
 
