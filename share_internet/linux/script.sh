aec528cade5f628923734d3a8c0a05b6409c0010
longkl <longkl@gmail.com>
2020-02-01 16:43:35 +0700

    add script to share internet via usb cable

---
+++ share_internet/linux/script.sh
@@ -0,0 +1,10 @@
+sudo su
+#wlan0 is my internet facing interface, eth5 is the BeagleBone USB connection
+PC_IFACE=wlp2s0
+BBB_IFACE=enx3403dea30136
+
+ifconfig $BBB_IFACE 192.168.6.1
+iptables --table nat --append POSTROUTING --out-interface $PC_IFACE -j MASQUERADE
+iptables --append FORWARD --in-interface $BBB_IFACE  -j ACCEPT
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
