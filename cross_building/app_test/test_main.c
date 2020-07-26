9a59b6213901ed5854a9044c40215151b022e92f
longkl <longkl@gmail.com>
2020-07-26 10:47:14 +0700

    add cross building script for beaglebone

---
+++ cross_building/app_test/test_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int x = 10 ;
+   char *name = "hello world";
+
+
+   printf("x = %d , message is %s\n", x, name);
+
+   return 0;
+}
---
+++ cross_building/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "starting configure path , cross compile for building process"
+echo export PATH=$PATH:/opt/gcc-arm-linux/bin
+echo make CROSS=arm-linux-gnueabihf- KERNEL=/media/longkl/DATA/linux/
+
+
+
+
+
