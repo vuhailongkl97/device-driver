443ac22a3ccf5fab45e098fa448bf4f60c65e8e9
long.kl.linux_mint <vuhailongkl97@gmail.com>
2022-07-02 11:28:19 +0700

    update iptables for internet sharing bbb

--- share_internet/linux/script.sh
+++ share_internet/linux/script.sh
@@ -6,5 +6,6 @@ BBB_IFACE=enx3403dea30136
 ifconfig $BBB_IFACE 192.168.7.1
 iptables --table nat --append POSTROUTING --out-interface $PC_IFACE -j MASQUERADE
 iptables --append FORWARD --in-interface $BBB_IFACE  -j ACCEPT
+iptables --append FORWARD --out-interface $BBB_IFACE  -j ACCEPT
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
